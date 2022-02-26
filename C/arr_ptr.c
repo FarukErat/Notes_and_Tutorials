d22d75b296488f301391a1fce4a4bb3593354acd
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-02-26 20:59:04 +0300

    Create arr_ptr.c

---
+++ C/arr_ptr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define ROW 3
+#define COL 5
+
+int main()
+{
+    int arr[ROW][COL];
+
+    for (int i = 0; i < ROW * COL; i++)
+    {
+        arr[i / COL][i % COL] = i;
+    }
+    for (int i = 0; i < ROW * COL; i++)
+    {
+        if (i % COL == 0)
+            printf("\n");
+        printf("%d ", arr[i / COL][i % COL]);
+    }
+
+    return 0;
+}
