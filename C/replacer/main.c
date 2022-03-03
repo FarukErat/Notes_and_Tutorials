babc9ed8ab62dcd7500066eb89cdd59b153e55d1
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-03-03 18:03:31 +0300

    Test case: the end of the string

--- C/replacer/main.c
+++ C/replacer/main.c
@@ -7,6 +7,8 @@ int main()
     char rep[] = "World";
     char with[] = "Universe";
     replacer(text, rep, with);
-    printf("%s", text); // prints "Hello Universe"
+    // "!" is to see if there is any character in the end of the string
+    // to make sure the code runs properly
+    printf("%s!", text); // prints "Hello Universe!"
     return 0;
 }
