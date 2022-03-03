9322b48c0a21952f4a524ecda6766157b801175f
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-03-03 18:05:38 +0300

    Change the function name from replacer to replace

--- C/replacer/main.c
+++ C/replacer/main.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include "replacer.h"
+#include "replace.h"
 
 int main()
 {
     char text[] = "Hello World";
     char rep[] = "World";
     char with[] = "Universe";
-    replacer(text, rep, with);
+    replace(text, rep, with);
     // "!" is to see if there is any character in the end of the string
     // to make sure the code runs properly
     printf("%s!", text); // prints "Hello Universe!"
