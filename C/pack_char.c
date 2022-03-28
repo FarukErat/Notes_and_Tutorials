d34535920516d9ddcbe10b30aef775611afe261f
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-03-28 23:15:18 +0300

    Update pack_char.c

--- C/pack_char.c
+++ C/pack_char.c
@@ -4,51 +4,65 @@
 
 typedef struct
 {
-    char left;
-    char right;
-} TwoChar;
+    char first;
+    char second;
+    char third;
+    char fourth;
+} FourChar;
 
-unsigned packChar(char ch1, char ch2);
+unsigned packChar(char ch1, char ch2, char ch3, char ch4);
 void displayBits(unsigned value);
-TwoChar unpackChar(unsigned value);
+FourChar unpackChar(unsigned value);
 
 int main()
 {
-    char ch1, ch2;
+    char ch1, ch2, ch3, ch4;
     printf("First character : ");
     ch1 = getche();
     printf("\nSecond character: ");
     ch2 = getche();
-    unsigned packed = packChar(ch1, ch2);
-    TwoChar unpacked = unpackChar(packed);
+    printf("\nThird character : ");
+    ch3 = getche();
+    printf("\nFourth character: ");
+    ch4 = getche();
+    unsigned packed = packChar(ch1, ch2, ch3, ch4);
+    FourChar unpacked = unpackChar(packed);
     printf("\n");
     printf("Bits of the characters:\n");
     displayBits(ch1);
     displayBits(ch2);
+    displayBits(ch3);
+    displayBits(ch4);
     printf("Bits of the packed value\n");
     displayBits(packed);
-    printf("First character : %c\n", unpacked.left);
-    printf("Second character: %c\n", unpacked.right);
+    printf("First character : %c\n", unpacked.first);
+    printf("Second character: %c\n", unpacked.second);
+    printf("Third character : %c\n", unpacked.third);
+    printf("Fourth character: %c\n", unpacked.fourth);
 
     return 0;
 }
 
-unsigned packChar(char ch1, char ch2)
+unsigned packChar(char ch1, char ch2, char ch3, char ch4)
 {
-    unsigned result = 0;
-    // shift ch1 to the left 16 bits and then OR it with ch2 to combine them into one unsigned int
-    result = (ch1 << CHAR_BIT * sizeof(unsigned) / 2) | ch2;
-    return result;
+    unsigned packed = 0;
+    packed = packed | ch4;
+    packed = packed | (ch3 << CHAR_BIT);
+    packed = packed | (ch2 << CHAR_BIT * 2);
+    packed = packed | (ch1 << CHAR_BIT * 3);
+    return packed;
 }
 
-TwoChar unpackChar(unsigned value)
+FourChar unpackChar(unsigned value)
 {
-    TwoChar result;
+    FourChar result;
     // the sizes of char and unsigned are different, however, it will NOT affect the result
     // it overflows and returns the same value as the first character
-    unsigned oneByte = 255;    // 2^8 - 1 = 00000000 00000000 00000000 11111111
-    result.left = value >> 16; // right shift 16 bits to eliminate the right character
-    result.right = value & oneByte;
+    unsigned oneByte = 255; // 2^8  - 2^0  = 00000000 00000000 00000000 11111111
+    result.first = value >> CHAR_BIT * 3 & oneByte;
+    result.second = value >> CHAR_BIT * 2 & oneByte;
+    result.third = value >> CHAR_BIT & oneByte;
+    result.fourth = value & oneByte;
     return result;
 }
 
