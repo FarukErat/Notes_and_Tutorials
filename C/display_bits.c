527b1c6b8f4d9255310ca490ffabacf9bfa29b76
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-03-28 13:17:14 +0300

    Update display_bits.c

--- C/display_bits.c
+++ C/display_bits.c
@@ -5,20 +5,19 @@ void displayBits(unsigned value);
 
 int main()
 {
-    unsigned a = 99996754, b = 12;
-    displayBits(a); // 00000101 11110101 11010100 01010010
-    displayBits(b); // 00000101 11110101 11010100 01010010
-    displayBits(a | b);
-    displayBits(a & b);
-    displayBits(a ^ b);
-    displayBits(~a);
+    unsigned a = 99996754, b = 30;
+    displayBits(a);     // 00000101 11110101 11010100 01010010
+    displayBits(b);     // 00000000 00000000 00000000 00011110
+    displayBits(a | b); // 00000101 11110101 11010100 01011110
+    displayBits(a & b); // 00000000 00000000 00000000 00010010
+    displayBits(a ^ b); // 00000101 11110101 11010100 01001100
+    displayBits(~a);    // 11111010 00001010 00101011 10101101
     return 0;
 }
 
 void displayBits(unsigned value)
 {
     // CHAR_BIT: number of bits in a byte
-    printf("%10d = ", value);
     unsigned c;
     // declare a variable of 32 bits(4 bytes) by shifting 1 by 31
     unsigned fourByte = 1 << CHAR_BIT * sizeof(unsigned) - 1; // 10000000 00000000 00000000 00000000
