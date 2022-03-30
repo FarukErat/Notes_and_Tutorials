a691206663f73c45c69ba9ddd903a5a041ba98e2
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-03-30 22:23:16 +0300

    Update delay.c

--- C/delay.c
+++ C/delay.c
@@ -3,24 +3,22 @@
 
 /**
  * @brief delays the program for a given amount of time in milliseconds
- * 
- * @param milliseconds 
+ *
+ * @param milliseconds
  */
 void delay(unsigned milliseconds)
 {
-    clock_t start_time = clock();
-    // loop until date exceeds called time + milliseconds
-    // therefore delay is milliseconds
-    while (clock() < start_time + milliseconds);
+    clock_t time_end = clock() + milliseconds;
+    while (clock() < time_end);
 }
 
 int main()
 {
-    time_t t;
-    time(&t);
+    time_t t;                  // time_t is a data type for time
+    time(&t);                  // time() returns the current time in seconds since the Epoch
     printf("%s\n", ctime(&t)); // ctime() returns a string of the current time and date.
-    delay(1000);
-    time(&t);
+    delay(1000);               // delays the program for 1 second
+    time(&t);                  // get the current time
     printf("%s\n", ctime(&t)); // day of week, month, day of month, hh:mm:ss yyyy
 
     return 0;
