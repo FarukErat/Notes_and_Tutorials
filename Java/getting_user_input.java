e2df667c53cdf9e8e269af90efc1ddb159a0f668
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-04-06 23:13:13 +0300

    Update getting_user_input.java

--- Java/getting_user_input.java
+++ Java/getting_user_input.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
 
 class MyClass {
+    // public to be accessible from everywhere
+    // static to be accessible without creating an object
     public static void main(String[] args) {
-        System.out.println("Enter something");
-        Scanner myVar = new Scanner(System.in);
-        System.out.println(myVar.nextLine());
-        myVar.close();
+        System.out.println("Enter something"); // print to console
+        Scanner myVar = new Scanner(System.in); // instantiate scanner object to read from console
+        System.out.println("You entered: " + myVar.nextLine());
+        myVar.close(); // close scanner object
     }
 }
