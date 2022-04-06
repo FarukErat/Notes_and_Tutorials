4261d33a7181a2fe18fb40a39543a9035eb13471
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-04-06 23:08:11 +0300

    Update getting_user_input.java

--- Java/getting_user_input.java
+++ Java/getting_user_input.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
 class MyClass {
-    public static void main(String[ ] args) {
+    public static void main(String[] args) {
+        System.out.println("Enter something");
         Scanner myVar = new Scanner(System.in);
-        System.out.println(myVar.nextLine());        
+        System.out.println(myVar.nextLine());
+        myVar.close();
     }
 }
