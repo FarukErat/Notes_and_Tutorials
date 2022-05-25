fcd15d1526e81bfa4561e79ae5b5e669235945e9
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-05-25 19:15:16 +0300

    Update store_funcs_in_a_list.cpp

--- Cpp/store_funcs_in_a_list.cpp
+++ Cpp/store_funcs_in_a_list.cpp
@@ -2,28 +2,28 @@
 
 using namespace std;
 
-void first(void)
+void first(int num1, int num2)
 {
-    cout << "first" << endl;
+    cout << "First function called with " << num1 << " and " << num2 << endl;
 }
 
-void second(void)
+void second(int num1, int num2)
 {
-    cout << "second" << endl;
+    cout << "Second function called with " << num1 << " and " << num2 << endl;
 }
 
-void third(void)
+void third(int num1, int num2)
 {
-    cout << "third" << endl;
+    cout << "Third function called with " << num1 << " and " << num2 << endl;
 }
 
 int main()
 {
     // create a list to store functions
-    void (*funcs[])(void) = {first, second, third};
-    funcs[1](); // second
-    funcs[0](); // first
-    funcs[2](); // third
+    void (*funcs[])(int, int) = {first, second, third};
+    funcs[1](0, 5); // call second function with 0 and 5
+    funcs[0](3, 1); // call first function with 3 and 1
+    funcs[2](7, 2); // call third function with 7 and 5
 
     return 0;
 }
