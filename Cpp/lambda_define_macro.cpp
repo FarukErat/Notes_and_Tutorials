289fb1ce60c7df97e4d42457be68380ff6fa8cec
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-05-28 04:28:59 +0300

    Create lambda_define_macro.cpp

---
+++ Cpp/lambda_define_macro.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#define day ((a) * 365)
+
+using namespace std;
+
+int main()
+{
+    int a;
+    a = 3;
+    cout << day << '\n';
+    a = 4;
+    cout << day << '\n';
+    return 0;
+}
+
+#undef b
