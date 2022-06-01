73af3dc6112e7b6af5a96d9fde1c0aa6c0543116
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-06-02 00:09:50 +0300

    Add files via upload

---
+++ Cpp/Othello_QT_QUI-main/README.md
@@ -0,0 +1,6 @@
+# Othello_QT_QUI
+For now, almost the same as [Othello](https://github.com/FarukErat/Othello) but
+this program will have more functionalities than it has.<br>
+Less feature but `GUI`<br>
+Written in `C++`<br>
+Built with `QT Creator`<br>
---
+++ Cpp/Othello_QT_QUI-main/othello/mainwindow.hpp
@@ -0,0 +1,94 @@
+#ifndef MAINWINDOW_HPP
+#define MAINWINDOW_HPP
+
+#include <vector>
+#include <iostream>
+
+#include <QtWidgets/QPushButton>
+#include <QMainWindow>
+#include <QStyle>
+#include <QLabel>
+
+#define SIZE 8
+#define BLACK 'B'
+#define WHITE 'W'
+#define EMPTY '-'
+#define LEGAL 'x'
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+struct coor
+{
+    int col;
+    int row;
+};
+
+// Define all the possible directions in which a player's move can flip
+// their adversary's tiles as constant (0 – the current row/column,
+// +1 – the next row/column, -1 – the previous row/column)
+const int DIRS[8][2] =
+{
+    {-1, -1},   {-1, 0},    {-1, 1},
+    {0, -1},                {0, 1},
+    {1, -1},    {1, 0},     {1, 1}
+};
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+    // bool return type functions
+    bool hasTileToFlip();
+    bool isLegal(coor);
+    bool isOnBoard(coor);
+
+    // void return type function
+    void clickToCoor();
+    void flipTiles(coor);
+    void marker();
+    void updateBtns();
+    void proccess(coor);
+    void game(); // not ready yet
+    void delayer(unsigned delay = 1000);
+    void switchTurn();
+    void cpuPlays(unsigned delay = 1000);
+    void humanPlays();
+
+    coor randomMove();
+
+    // getter functions
+    char getBoard(int row, int col);
+    bool getMarking();
+    int getGameMode();
+    char getTurn();
+    char getOpponent();
+    char getHumanSide();
+
+    // setter functions
+    void setBoard(int row, int col, char value);
+    void setMarking(bool);
+    void setGameMode(int);
+    void setTurn(char);
+    void setHumanSide(char);
+private:
+    Ui::MainWindow *ui;
+    QPushButton *buttons[SIZE][SIZE];
+    QLabel *labels[SIZE][SIZE];
+
+    bool marking = true;
+    bool delayGuard = true;
+    int gameMode = 2;
+    char board[SIZE][SIZE];
+    char turn = BLACK;
+    char opponent = WHITE;
+    char humanSide = BLACK;
+
+};
+
+#endif // MAINWINDOW_HPP
