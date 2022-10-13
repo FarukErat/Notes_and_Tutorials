7b42e6e9843a3f2d4af85749876a112f0cb13129
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-10-13 23:44:45 +0300

    Add files via upload

---
+++ Web/SoloLearn/#hackerterminal/script.js
@@ -0,0 +1,13 @@
+var x = '' ; function text() { x = list.value ; 
+    if (x == 'List') { hack.innerHTML = 'Commands<br>List<br>Connect<br>Engage<br>Join Anonymous<br>Reveal Anonymous<br>Hack the White House<br>Hack the Planet'} 
+    else if (x == 'Connect') { hack.innerHTML = 'CONNECTING IN....<img src="https://media3.giphy.com/media/thNsW0HZ534DC/giphy.gif"height="350"width="350">'} 
+    else if (x == 'Engage') { hack.innerHTML = 'HACKING SYSTEM....<img src="https://media.giphy.com/media/zXmbOaTpbY6mA/giphy.gif"height="300"width="350">Mission Accomplished'} 
+    else if (x == 'Join Anonymous') { hack.innerHTML = 'Welcome to Anonymous....<img src="https://media2.giphy.com/media/lwAyIZrVNYfio/giphy.gif "height="300"width="350"><br>Sorry its not that easy'} 
+    else if (x == 'Reveal Anonymous') { hack.innerHTML = '<img src="https://www.gifpal.com/static/uimages/4f29a75b31158se_1440861110.gif "height="300"width="350">NEVER'} 
+    else if (x == 'Hack the White House') { hack.innerHTML = '<img src="http://78.media.tumblr.com/9cebcec899889b81f2adea7b5486fdbb/tumblr_ne8fbcYjVR1ru5h8co1_500.gif"height="330"width="350">Accomplished Миссия выполнена '} 
+    else if (x == 'Hack the Planet') { hack.innerHTML = '<img src="https://media.giphy.com/media/soV2cf4ylKhDG/giphy.gif"height="330"width="350">PLANET HACKED '} 
+    
+    }
+    
+    function clearInput(element){ element.value=""; }
+    
