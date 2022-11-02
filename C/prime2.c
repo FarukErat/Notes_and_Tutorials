e8d8d47f0aa22779b5fc932b40cdc4e91100d13d
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-11-03 02:11:20 +0300

    Update prime2.c

--- C/prime2.c
+++ C/prime2.c
@@ -8,6 +8,7 @@ int main()
     int lastDivider = 1;
     int power = 0;
 
+    // get the number from the user and check if the assignment was successful
     printf("Prime Factors of ");
     if (scanf("%lld", &number) != 1)
     {
@@ -17,59 +18,60 @@ int main()
         return 1;
     }
 
-    if (number == -1)
+    // special case for -1, 0 and 1
+    if (number == -1 || number == 0 || number == 1)
     {
         printf("-------------\n");
-        printf("-1\n");
+        printf("%d\n", number);
         printf("-------------\n");
         printf("Press any key to exit...");
         getch();
         return 0;
     }
 
+    // convert negative numbers to positive and print the sign
     printf("-------------");
     if (number < 0)
     {
         number = -number;
         printf("\n-1");
     }
-    switch (number)
+
+    // main loop
+    while (number != 1)
     {
-    case 0:
-        printf("\n0");
-        break;
-    case 1:
-        printf("\n1");
-        break;
-    default:
-        while (number != 1)
+        // check if the divider is a factor of the number
+        if (number % divider == 0)
         {
-            if (number % divider == 0)
-            {
-                number = number / divider;
-                if (divider == lastDivider)
-                    power++;
-                else
-                {
-                    lastDivider = divider;
-                    if (power > 1)
-                        printf(" ^ %d", power);
-                    printf("\n%d", divider);
-                    power = 1;
-                }
-                if (number == 1)
-                {
-                    if (power > 1)
-                        printf(" ^ %d", power);
-                }
-            }
+            // if so divide the number by the divider
+            number = number / divider;
+            // increase the power of the divider if it is the same as the last divider
+            if (divider == lastDivider)
+                power++;
             else
-                divider++;
+            {
+                // if not, there is a new divisor
+                // update the last divider and print the exponent of the previous divider
+                lastDivider = divider;
+                if (power > 1)
+                    printf(" ^ %d", power);
+                printf("\n%d", divider);
+                // since the divider is a new divisor, the power is 1
+                power = 1;
+            }
         }
-        break;
+        // iterate the divider
+        else
+            divider++;
     }
+
+    // print the last exponent if not 1
+    if (power > 1)
+        printf(" ^ %d", power);
+
     printf("\n-------------");
     printf("\nPress any key to exit...");
     getch();
+
     return 0;
 }
