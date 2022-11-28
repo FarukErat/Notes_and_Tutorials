3ebf7fa5de24179681f42cf31d5853b0d7222076
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-11-29 00:48:57 +0300

    Create operator_overloading.dart

---
+++ Dart/operator_overloading.dart
@@ -0,0 +1,23 @@
+class Vector {
+  final int x, y;
+
+  Vector(this.x, this.y);
+
+  Vector operator +(Vector v) => Vector(x + v.x, y + v.y);
+  Vector operator -(Vector v) => Vector(x - v.x, y - v.y);
+
+  @override
+  bool operator ==(Object other) =>
+      other is Vector && x == other.x && y == other.y;
+
+  @override
+  int get hashCode => Object.hash(x, y);
+}
+
+void main() {
+  final v = Vector(2, 3);
+  final w = Vector(2, 2);
+
+  print(v + w == Vector(4, 5));
+  print(v - w == Vector(0, 1));
+}
