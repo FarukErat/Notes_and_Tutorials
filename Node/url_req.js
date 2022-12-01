bbaf982ebcdcde19da00daab1ddc47029a67e88a
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-12-01 21:38:28 +0300

    Create url_req.js

---
+++ Node/url_req.js
@@ -0,0 +1,7 @@
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(req.url);
+    res.end();
+}).listen(8080);
+console.log('Server running at http://127.0.0.1:8080/');
