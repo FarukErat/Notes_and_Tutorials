0fcba2988027efbab68f5f00c22f6c7bef839fc7
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-12-06 01:04:23 +0300

    Create bisection_method.py

---
+++ Python/bisection_method.py
@@ -0,0 +1,34 @@
+def bisection(f, lower, upper, tol=1e-8, maxiter=100):
+    """Find root of f(x) using bisection method.
+
+    Parameters
+    ----------
+    f : function
+        Function to find root of.
+    a, b : float
+        Interval to search for root.
+    tol : float
+        Tolerance for stopping criterion.
+    maxiter : int
+        Maximum number of iterations.
+
+    Returns
+    -------
+    x : float
+        Estimated root location.
+    """
+    if f(upper)*f(lower) > 0:
+        raise ValueError("f(a) and f(b) must have different signs")
+    for i in range(maxiter):
+        mid = (lower + upper)/2
+        fm = f(mid)
+        if abs(fm) < tol:
+            return mid
+        elif fm < 0:
+            lower = mid
+        else:
+            upper = mid
+    raise RuntimeError("Maximum number of iterations exceeded")
+
+
+print(bisection(lambda x: x**2 - 8, 0, 4))
