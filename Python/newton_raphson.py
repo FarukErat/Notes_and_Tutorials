7e91e0ae78f70522b42c36f6a5a19cd9e02f2e07
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-12-06 01:26:37 +0300

    Update newton_raphson.py

--- Python/newton_raphson.py
+++ Python/newton_raphson.py
@@ -1,6 +1,6 @@
 def new_rap(f, x0=0, tol=1e-8, max_iter=100):
     x = x0
-    f_prime =  lambda x: (f(x + tol) - f(x)) / tol
+    def f_prime(x): return (f(x + tol) - f(x)) / tol
     for i in range(max_iter):
         x = x - f(x) / f_prime(x)
         if abs(f(x)) < tol:
