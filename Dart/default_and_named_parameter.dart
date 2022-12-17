4c9d9babfca949c3f38d890613aeedeb29621c8a
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-12-17 21:30:14 +0300

    Update default_and_named_parameter.dart

--- Dart/default_and_named_parameter.dart
+++ Dart/default_and_named_parameter.dart
@@ -13,6 +13,10 @@ void foo3({int num = 0}) {
   print("foo3 $num");
 }
 
+void foo4(int num1, {int? num2}) {
+  print("foo4 $num1 $num2");
+}
+
 void main() {
   // foo1(); // error
   foo1(1); // foo1 1
@@ -25,4 +29,9 @@ void main() {
   foo3(); // foo3 0
   // foo3(1); // error
   foo3(num: 1); // foo3 1
+
+  foo4(1); // foo4 1 null
+  // foo4(1, 2); // error
+  // foo4(num2: 1); // error
+  foo4(1, num2: 2); // foo4 1 2
 }
