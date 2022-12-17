1162a1b71972410b367612e8ec1bb96d649ae3a8
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-12-17 14:17:40 +0300

    Create default_and_named_parameter.dart

---
+++ Dart/default_and_named_parameter.dart
@@ -0,0 +1,24 @@
+void foo1(int num) {
+  print("foo1 $num");
+}
+
+// below is wrong because default value is not allowed as named parameter
+// void foo1(int num = 0) { print("foo1 $num");}
+
+void foo2({int? num}) {
+  print("foo2 $num");
+}
+
+void foo3({int num = 0}) {
+  print("foo3 $num");
+}
+
+void main() {
+  foo1(1); // foo1 1
+
+  foo2(); // foo2 null
+  foo2(num: 1); // foo2 1
+
+  foo3(); // foo3 0
+  foo3(num: 1); // foo3 1
+}
