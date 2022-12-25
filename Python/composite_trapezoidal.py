8dfe2b9af2e539e8210d78190e8e4073df3885f4
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-12-25 22:56:14 +0300

    Create composite_trapezoidal.py

---
+++ Python/composite_trapezoidal.py
@@ -0,0 +1,16 @@
+# an implementation of composite trapezoidal rule for integration
+
+def integrate(f, start, finish, n=1000):
+    h = (finish - start) / n
+    sum = f(start) + f(finish)
+    for i in range(1, n):
+        sum += 2 * f(start + h * i)
+    return sum * h / 2
+
+
+def f(x):
+    return x**3 + 2 * x**2 + 3 * x + 4
+
+
+print(integrate(f, 0, 1))  # output:     6.416667249999999
+print(77 / 12)             # real value: 6.416666666666667
