40300ca36e2cb2ebceff45fbe48b823aa7b68e89
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2023-01-10 00:02:36 +0300

    Create decorator_5.py

---
+++ Python/decorator_5.py
@@ -0,0 +1,30 @@
+# taken from https://www.datacamp.com/tutorial/decorators-python
+
+
+def decorator_maker_with_arguments(decorator_arg1, decorator_arg2, decorator_arg3):
+    def decorator(func):
+        def wrapper(function_arg1, function_arg2, function_arg3):
+            "This is the wrapper function"
+            print("The wrapper can access all the variables\n"
+                  "\t- from the decorator maker: {0} {1} {2}\n"
+                  "\t- from the function call: {3} {4} {5}\n"
+                  "and pass them to the decorated function"
+                  .format(decorator_arg1, decorator_arg2, decorator_arg3,
+                          function_arg1, function_arg2, function_arg3))
+            return func(function_arg1, function_arg2, function_arg3)
+
+        return wrapper
+
+    return decorator
+
+
+pandas = "Pandas"
+
+
+@decorator_maker_with_arguments(pandas, "Numpy", "Scikit-learn")
+def decorated_function_with_arguments(function_arg1, function_arg2, function_arg3):
+    print("This is the decorated function and it only knows about its arguments: {0}"
+          " {1}" " {2}".format(function_arg1, function_arg2, function_arg3))
+
+
+decorated_function_with_arguments(pandas, "Science", "Tools")
