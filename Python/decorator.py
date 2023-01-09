256b7dd6f278a7e2adb5775c3c635e5fec952fbc
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2023-01-09 23:50:56 +0300

    Create decorator.py

---
+++ Python/decorator.py
@@ -0,0 +1,25 @@
+def uppercase_decorator(function):
+    def wrapper():
+        func = function()
+        make_uppercase = func.upper()
+        return make_uppercase
+
+    return wrapper
+
+
+def say_hi_1():
+    return 'hello there'
+
+
+decorate = uppercase_decorator(say_hi_1)
+print(decorate())
+# HELLO THERE
+
+
+@uppercase_decorator
+def say_hi_2():
+    return 'hello there'
+
+
+print(say_hi_2())
+# HELLO THERE
