c2e900ac6db39bc35ceaba81eee15a603325e646
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2023-01-10 01:37:36 +0300

    Create try_catch_nested.py

---
+++ Python/try_catch_nested.py
@@ -0,0 +1,12 @@
+# taken from https://www.w3schools.com/python/python_try_except.asp
+
+try:
+  f = open("demofile.txt")
+  try:
+    f.write("Lorum Ipsum")
+  except:
+    print("Something went wrong when writing to the file")
+  finally:
+    f.close()
+except:
+  print("Something went wrong when opening the file")
