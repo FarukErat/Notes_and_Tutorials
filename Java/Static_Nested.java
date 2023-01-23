214d903fa7b6c066171397297613b4c1d865f69c
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2023-01-24 00:35:02 +0300

    Update Static_Nested.java

--- Java/Static_Nested.java
+++ Java/Static_Nested.java
@@ -2,10 +2,19 @@ public class LearnStatic {
 
     private static String str = "GeeksforGeeks";
 
+    // this will be executed when the class is loaded into the memory
+    // and before the main method is called
     static {
         System.out.println("LearnStatic: Static block initialized.");
     }
 
+    // this will not be executed when the class is loaded into the memory
+    // but can be called with instanciation of the class
+    static void disp() {
+        System.out.println("LearnStatic: Static method called.");
+    }
+
+    // this will be executed when the class is instanciated
     LearnStatic() {
         System.out.println("LearnStatic: Constructor called.");
     }
