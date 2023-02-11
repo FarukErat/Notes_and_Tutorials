223178ed2c1ffec82f696cba382d0d9f9c97a40a
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2023-02-11 20:14:24 +0300

    Update get_set.cs

--- C#/get_set.cs
+++ C#/get_set.cs
@@ -2,31 +2,26 @@ class Program
 {
     static void Main()
     {
-        // if (fptr = fileopen(filename) == NULL)
-        var Jhon = new Person();
-        Console.WriteLine(Jhon.Name);
-        Jhon.Name = "Jhon"; // Jhon.setName("Jhon");
-        Console.WriteLine(Jhon.Name);
-        Console.WriteLine(Jhon.Name = "Tom");
-        Console.WriteLine(Jhon.Age);
-        Jhon.Age = 25;
-        Console.WriteLine(Jhon.Age);
-        Console.WriteLine(Jhon.DaysLived);
-        Jhon.Age++;
-        Console.WriteLine(Jhon.DaysLived);
+        var Jhon = new Person();               // constructor called
+        Jhon.Name = "Jhon";                    // set called
+        Console.WriteLine(Jhon.Name = "Tom");  // set called, "Tom", (get is not called, "Tom" is passed to Console.WriteLine)
+        Jhon.Age = 25;                         // set called
+        Jhon.Age = Jhon.Age + 1;               // get called, set called
+        Jhon.Age++;                            // get called, set called
     }
 }
 
 class Person
 {
+    public Person() { Console.WriteLine("constructor called"); }
     private int age = 0;
-    private string name = "Unknown";
+    private string name = "unknown";
     private int daysLived => age * 365;
     public string Name
     {
         get { Console.WriteLine("get called"); return name; }
         set { Console.WriteLine("set called"); name = value; }
     }
-    public int Age { get { return age; } set { if (value <= 0) age = 0; else age = value; } }
-    public int DaysLived { get { return daysLived; } } // readonly
+    public int Age { get { Console.WriteLine("get called"); return age; } set { Console.WriteLine("set called"); if (value <= 0) age = 0; else age = value; } }
+    public int DaysLived { get { Console.WriteLine("get called"); return daysLived; } }
 }
