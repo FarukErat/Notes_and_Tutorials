c63834d6b8f0807ea229ce431b80363587da6a59
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2023-04-07 18:55:13 +0300

    Create badget_glitch.js

---
+++ Node/badget_glitch.js
@@ -0,0 +1,25 @@
+const https = require("https");
+
+const url = "https://visitor-badge.laobi.icu/badge?page_id=RandomID";
+const regex = /<text.*>\s*(\d+)\s*<\/text>/;
+const delayMs = 10;
+let counter = 0;
+
+setInterval(() => {
+  https.get(url, (res) => {
+    let data = "";
+    res.on("data", (chunk) => {
+      data += chunk;
+    });
+    res.on("end", () => {
+      const match = data.match(regex);
+      if (match) {
+        counter++;
+        const visitors = match[1];
+        process.stdout.write(
+          `Total Visitors: ${visitors}, New Visitors: ${counter}\r`
+        );
+      }
+    });
+  });
+}, delayMs);
