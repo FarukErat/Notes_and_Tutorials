d8cb66c8f9d786bf1d9adc743186f3e97d3e8655
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2023-04-10 18:19:05 +0300

    Update change_func_def.c

--- C/community/change_func_def.c
+++ C/community/change_func_def.c
@@ -1,35 +1,49 @@
 #include <stdio.h>
 
-// declaration of a function pointer of return type void and no arguments
+// declaration
+// of
+// a function pointer of return type void and no arguments
+// and
+// functions of return type void and no arguments
 void (*func_ptr)();
+void alpha();
+void beta();
+void charlie();
+
+int main()
+{
+    func_ptr(); // alpha
+    func_ptr(); // beta
+    func_ptr(); // charlie
+    func_ptr(); // alpha
+    func_ptr(); // beta
+    func_ptr(); // charlie
+
+    return 0;
+}
+
+// definition
+// of
+// a function pointer of return type void and no arguments
+// and
+// functions of return type void and no arguments
+
+void (*func_ptr)() = alpha; // func_ptr is now pointing to alpha
 
 void alpha()
 {
     printf("alpha\n");
+    func_ptr = beta; // func_ptr is now pointing to charlie
 }
 
 void beta()
 {
     printf("beta\n");
-    func_ptr = alpha; // func_ptr is now pointing to alpha
+    func_ptr = charlie; // func_ptr is now pointing to alpha
 }
 
 void charlie()
 {
     printf("charlie\n");
-    func_ptr = beta; // func_ptr is now pointing to beta
-}
-
-// func_ptr is initialized not declared
-void (*func_ptr)() = charlie;
-
-int main()
-{
-    func_ptr(); // charlie
-    func_ptr(); // beta
-    func_ptr(); // alpha
-    func_ptr(); // alpha
-    func_ptr(); // alpha
-
-    return 0;
+    func_ptr = alpha; // func_ptr is now pointing to beta
 }
