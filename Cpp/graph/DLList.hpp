716b3aad431ed2d812ccf7dd0aec1761d1968cd0
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2023-04-25 16:43:01 +0300

    Create DLList.hpp

---
+++ Cpp/graph/DLList.hpp
@@ -0,0 +1,327 @@
+#pragma once
+#ifndef DLLIST
+#define DLLIST
+
+#include <iostream>
+
+using std::initializer_list;
+using std::ostream;
+using std::istream;
+using std::out_of_range;
+
+template <typename T>
+class Node {
+public:
+    T data;
+    Node<T>* next = nullptr;
+    Node<T>* prev = nullptr;
+};
+template <typename T>
+class DLList {
+private:
+    int size = 0;
+    Node<T>* head = nullptr;
+    Node<T>* tail = nullptr;
+    bool boundCheck(int index);
+
+public:
+    T input;
+    DLList() {};
+    DLList(initializer_list<T> const l);
+    ~DLList();
+
+    int getSize() const { return size; }; /* read-only */
+
+    void insert(T data, int index);
+    void append(T data);
+    void remove(int index);
+    bool removeByVal(T data);
+    bool contains(T data);
+
+    T pop();
+
+    T& operator[](int index);
+
+    void operator=(DLList<T>& list);
+    void operator=(initializer_list<T> const l);
+
+    void operator+=(T data);
+    void operator+=(DLList<T>& list);
+    void operator+=(initializer_list<T> const l);
+
+    template <typename U>
+    friend ostream& operator<<(ostream& out, DLList<U>& list);
+    template <typename U>
+    friend istream& operator>>(istream& in, DLList<U>& list);
+
+    class iterator {
+    private:
+        Node<T>* ptr;
+    public:
+        iterator(Node<T>* ptr) : ptr(ptr) {};
+        iterator& operator++() {
+            ptr = ptr->next;
+            return *this;
+        };
+        iterator& operator--() {
+            ptr = ptr->prev;
+            return *this;
+        };
+        bool operator==(iterator& other) {
+            return ptr == other.ptr;
+        };
+        bool operator!=(iterator& other) {
+            return ptr != other.ptr;
+        };
+        T& operator*() {
+            return ptr->data;
+        };
+    };
+    iterator begin();
+    iterator end();
+};
+
+template<typename T>
+typename DLList<T>::iterator DLList<T>::begin() {
+    return iterator(head);
+}
+
+template<typename T>
+typename DLList<T>::iterator DLList<T>::end() {
+    return iterator(nullptr);
+}
+
+template <typename T>
+bool DLList<T>::boundCheck(int index) {
+    Node<T>* iter = head;
+    for (int i = 0; i < index; i++) {
+        if (iter == nullptr) {
+            return false;
+        }
+        iter = iter->next;
+    }
+    return true;
+};
+
+template <typename T>
+DLList<T>::DLList(initializer_list<T> const l) {
+    for (auto i : l) {
+        this->append(i);
+    }
+};
+template <typename T>
+DLList<T>::~DLList() {
+    Node<T>* ptr = head;
+    while (ptr != nullptr) {
+        Node<T>* temp = ptr;
+        ptr = ptr->next;
+        delete temp;
+    }
+};
+
+template <typename T>
+void DLList<T>::insert(T data, int index) {
+    Node<T>* newNode = new Node<T>;
+    newNode->data = data;
+    if (head == nullptr) {
+        newNode->next = nullptr;
+        newNode->prev = nullptr;
+        head = newNode;
+        tail = newNode;
+        size++;
+        return;
+    }
+    if (index == 0) {
+        newNode->next = head;
+        newNode->prev = nullptr;
+        head->prev = newNode;
+        head = newNode;
+        size++;
+        return;
+    }
+    else if (index == size) {
+        newNode->prev = tail;
+        newNode->next = nullptr;
+        tail->next = newNode;
+        tail = newNode;
+        size++;
+        return;
+    }
+    else {
+        newNode->next = nullptr;
+        newNode->prev = nullptr;
+        Node<T>* ptr = nullptr;
+        if (index < size / 2) {
+            ptr = head;
+            for (int i = 0; i < index; i++) {
+                ptr = ptr->next;
+            }
+        }
+        else {
+            ptr = tail;
+            for (int i = size - 1; i > index; i--) {
+                ptr = ptr->prev;
+            }
+        }
+        newNode->next = ptr;
+        newNode->prev = ptr->prev;
+        ptr->prev->next = newNode;
+        ptr->prev = newNode;
+        size++;
+    }
+};
+template <typename T>
+void DLList<T>::append(T data) {
+    insert(data, size);
+}
+template <typename T>
+void DLList<T>::remove(int index) {
+    if (!boundCheck(index))
+        return;
+    Node<T>* ptr;
+    if (size == 1) {
+        delete head;
+        head = nullptr;
+        tail = nullptr;
+        size--;
+        return;
+    }
+    if (index < size / 2) {
+        ptr = head;
+        for (int i = 0; i < index; i++) {
+            ptr = ptr->next;
+        }
+    }
+    else {
+        ptr = tail;
+        for (int i = size - 1; i > index; i--) {
+            ptr = ptr->prev;
+        }
+    }
+    if (ptr->prev == nullptr) {
+        head = ptr->next;
+        head->prev = nullptr;
+    }
+    if (ptr->next == nullptr) {
+        tail = ptr->prev;
+        tail->next = nullptr;
+    }
+    if (ptr->prev != nullptr && ptr->next != nullptr) {
+        ptr->prev->next = ptr->next;
+        ptr->next->prev = ptr->prev;
+    }
+    delete ptr;
+    size--;
+};
+
+template <typename T>
+bool DLList<T>::removeByVal(T data) {
+    Node<T>* ptr = head;
+    for (int i = 0; i < size; i++) {
+        if (ptr->data == data) {
+            remove(i);
+            return true;
+        }
+        ptr = ptr->next;
+    }
+    return false;
+};
+
+template <typename T>
+bool DLList<T>::contains(T data) {
+    Node<T>* ptr = head;
+    for (int i = 0; i < size; i++) {
+        if (ptr->data == data) {
+            return true;
+        }
+        ptr = ptr->next;
+    }
+    return false;
+};
+
+template <typename T>
+T DLList<T>::pop() {
+    T data = tail->data;
+    remove(size - 1);
+    return data;
+};
+
+/* I N D E X */
+template <typename T>
+T& DLList<T>::operator[](int index) {
+    if (index >= size || boundCheck(index) == false) {
+        throw out_of_range("Index out of bound.");
+    }
+    Node<T>* ptr;
+    if (index < size / 2) {
+        ptr = head;
+        for (int i = 0; i < index; i++) {
+            ptr = ptr->next;
+        }
+    }
+    else {
+        ptr = tail;
+        for (int i = size - 1; i > index; i--) {
+            ptr = ptr->prev;
+        }
+    }
+    return ptr->data;
+};
+
+/* A S S I G N M E N T */
+template <typename T>
+void DLList<T>::operator=(DLList<T>& list) {
+    if (this == &list)
+        return;
+    this->~DLList();
+    DLList<T>* temp = new (this) DLList<T>(); /* implicitly reconstructing */
+    for (int i = 0; i < list.getSize(); i++) {
+        this->append(list[i]);
+    }
+};
+template <typename T>
+void DLList<T>::operator=(initializer_list<T> const l) {
+    this->~DLList();
+    DLList<T>* temp = new (this) DLList<T>();
+    for (auto i : l) {
+        this->append(i);
+    }
+};
+
+/* P L U S   E Q U A L S */
+template <typename T>
+void DLList<T>::operator+=(T data) {
+    this->append(data);
+};
+template <typename T>
+void DLList<T>::operator+=(DLList<T>& list) {
+    for (int i = 0; i < list.getSize(); i++) {
+        this->append(list[i]);
+    }
+};
+template <typename T>
+void DLList<T>::operator+=(initializer_list<T> const l) {
+    for (auto i : l) {
+        this->append(i);
+    }
+};
+
+/* L E F T   S H I F T */
+template <typename T>
+ostream& operator<<(ostream& out, DLList<T>& list) {
+    for (int i = 0; i < list.getSize(); i++) {
+        out << list[i] << ' ';
+    }
+    return out;
+};
+
+/* R I G H T   S H I F T */
+template <typename T>
+istream& operator>>(istream& in, DLList<T>& list) {
+    T data;
+    in >> data;
+    list.append(data);
+    return in;
+};
+
+#endif // DLLIST
