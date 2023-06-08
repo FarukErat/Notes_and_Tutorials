eb7dd7ceeeb83fd5226ec60d272f2140afe5a494
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2023-06-08 21:54:42 +0300

    Update person_controller.dart

--- Dart/sample_project/controllers/person_controller.dart
+++ Dart/sample_project/controllers/person_controller.dart
@@ -9,57 +9,38 @@ class PersonController {
 
   // Add a new person to the database with a random ID
   static void addPerson(Person person) {
-    if (peopleDB == null) {
-      peopleDB = [person];
-    } else {
-      peopleDB!.add(person);
-    }
+    peopleDB.add(person);
   }
 
   // Update an existing person in the database
   static void updatePerson(String id, Person updatedPerson) {
-    if (peopleDB != null) {
-      int index = peopleDB!.indexWhere((person) => person.id == id);
-      if (index >= 0) {
-        peopleDB![index] = updatedPerson;
-      }
+    int index = peopleDB.indexWhere((person) => person.id == id);
+    if (index >= 0) {
+      peopleDB[index] = updatedPerson;
     }
   }
 
   // Delete an existing person from the database
   static void deletePerson(String id) {
-    if (peopleDB != null) {
-      peopleDB!.removeWhere((person) => person.id == id);
-    }
+    peopleDB.removeWhere((person) => person.id == id);
   }
 
   // Find a person by ID
   static Person? getPersonById(String id) {
-    if (peopleDB != null) {
-      return peopleDB!.firstWhere((person) => person.id == id);
-    }
-    return null;
+    return peopleDB.firstWhere((person) => person.id == id);
   }
 
   // Find people by name
   static List<Person>? getPeopleByName(String name) {
-    if (peopleDB != null) {
-      return peopleDB!
-          .where((person) =>
-              person.name!.toLowerCase().contains(name.toLowerCase()))
-          .toList();
-    }
-    return null;
+    return peopleDB
+        .where(
+            (person) => person.name!.toLowerCase().contains(name.toLowerCase()))
+        .toList();
   }
 
   // Find people by hobby
   static List<Person>? getPeopleByHobby(String hobby) {
-    if (peopleDB != null) {
-      return peopleDB!
-          .where((person) => person.hobbies!.contains(hobby))
-          .toList();
-    }
-    return null;
+    return peopleDB.where((person) => person.hobbies!.contains(hobby)).toList();
   }
 
   // Find all children of a person by ID
