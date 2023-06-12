be2f5d7b50c9d4a2869386df1b1d8ea1a9d0ac98
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2023-06-13 01:31:36 +0300

    "iat" field

--- JavaScript/jwt-api/jwt_jws-jwe.js
+++ JavaScript/jwt-api/jwt_jws-jwe.js
@@ -4,22 +4,17 @@ const crypto = require("crypto");
 require("dotenv").config();
 
 const app = express();
-const port = 3001;
+const port = 3002;
 
 app.use(express.json());
 
-const secret = crypto
-  .createHash("sha256")
-  .update(String(process.env.SECRET))
-  .digest("base64")
-  .substring(0, 32);
-
-const toJwt = (data) => jwt.sign(data, process.env.JWT_SECRET);
-const fromJwt = (data) => jwt.verify(data, process.env.JWT_SECRET);
+const jwsSecret = process.env.JWS_SECRET;
+const toJwt = (data) => jwt.sign(data, jwsSecret);
+const fromJwt = (data) => jwt.verify(data, jwsSecret);
 
 app.post("/create-jws", async (req, res) => {
   try {
-    const token = await toJwt(req.body);
+    const token = toJwt(req.body);
     res.status(200).json({ token: token });
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -36,11 +31,27 @@ app.post("/verify-jws", async (req, res) => {
   }
 });
 
+//! JWE
+
+// resize secret
+// since createCipheriv and createDecipheriv requires string of 32 characters
+const jweSecret = crypto
+  .createHash("sha256")
+  .update(String(process.env.JWE_SECRET))
+  .digest("base64")
+  .substring(0, 32);
+
 function encrypt(text) {
   return new Promise((resolve, reject) => {
     const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv("aes-256-cbc", secret, iv);
-    let encrypted = cipher.update(text, "utf8", "hex");
+    const cipher = crypto.createCipheriv("aes-256-cbc", jweSecret, iv);
+
+    // add "iat" field manually because it will not be added automatically like jws
+    // since the payload will not be a json but a random encrypted string in the jwt.sign()
+    const payload = JSON.parse(text);
+    payload["iat"] = Math.floor(Date.now() / 1000);
+
+    let encrypted = cipher.update(JSON.stringify(payload), "utf8", "hex");
     encrypted += cipher.final("hex");
     resolve(iv.toString("hex") + encrypted);
   });
@@ -49,7 +60,7 @@ function encrypt(text) {
 function decrypt(encryptedText) {
   return new Promise((resolve, reject) => {
     const iv = Buffer.from(encryptedText.slice(0, 32), "hex");
-    const decipher = crypto.createDecipheriv("aes-256-cbc", secret, iv);
+    const decipher = crypto.createDecipheriv("aes-256-cbc", jweSecret, iv);
     let decrypted = decipher.update(encryptedText.slice(32), "hex", "utf8");
     decrypted += decipher.final("utf8");
     resolve(decrypted);
@@ -59,7 +70,7 @@ function decrypt(encryptedText) {
 app.post("/create-jwe", async (req, res) => {
   try {
     const tokenData = JSON.stringify(req.body);
-    const encryptedToken = await encrypt(tokenData, process.env.SECRET);
+    const encryptedToken = await encrypt(tokenData, jweSecret);
     const token = toJwt(encryptedToken);
     res.status(200).json({ token });
   } catch (error) {
@@ -71,7 +82,7 @@ app.post("/verify-jwe", async (req, res) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     const encryptedToken = fromJwt(token);
-    const decryptedToken = await decrypt(encryptedToken, process.env.SECRET);
+    const decryptedToken = await decrypt(encryptedToken, jweSecret);
     const decoded = JSON.parse(decryptedToken);
     res.status(200).json(decoded);
   } catch (error) {
