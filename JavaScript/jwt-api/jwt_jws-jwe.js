7217bf19a3bb6101f62b4e679fedb136ba990518
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2023-06-12 05:03:01 +0300

    Update jwt_jws-jwe.js

--- JavaScript/jwt-api/jwt_jws-jwe.js
+++ JavaScript/jwt-api/jwt_jws-jwe.js
@@ -1,75 +1,84 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const crypto = require('crypto');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+require("dotenv").config();
 
 const app = express();
 const port = 3001;
 
-require('dotenv').config();
 app.use(express.json());
 
-app.post('/create-jws', async (req, res) => {
-    try {
-        const token = jwt.sign(req.body, process.env.JWT_SECRET);
-        res.status(200).json({ token: token });
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
+const secret = crypto
+  .createHash("sha256")
+  .update(String(process.env.SECRET))
+  .digest("base64")
+  .substring(0, 32);
+
+const toJwt = (data) => jwt.sign(data, process.env.JWT_SECRET);
+const fromJwt = (data) => jwt.verify(data, process.env.JWT_SECRET);
+
+app.post("/create-jws", async (req, res) => {
+  try {
+    const token = await toJwt(req.body);
+    res.status(200).json({ token: token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
-app.post('/verify-jws', async (req, res) => {
-    try {
-        const token = req.headers.authorization.split(' ')[1];
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        res.status(200).json(decoded)
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
+app.post("/verify-jws", async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decoded = fromJwt(token);
+    res.status(200).json(decoded);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
-// Encryption function using AES algorithm
-function encrypt(text, secretKey) {
-    const cipher = crypto.createCipher('aes-256-cbc', secretKey);
-    let encrypted = cipher.update(text, 'utf8', 'hex');
-    encrypted += cipher.final('hex');
-    return encrypted;
+function encrypt(text) {
+  return new Promise((resolve, reject) => {
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv("aes-256-cbc", secret, iv);
+    let encrypted = cipher.update(text, "utf8", "hex");
+    encrypted += cipher.final("hex");
+    resolve(iv.toString("hex") + encrypted);
+  });
 }
 
-// Decryption function using AES algorithm
-function decrypt(encryptedText, secretKey) {
-    const decipher = crypto.createDecipher('aes-256-cbc', secretKey);
-    let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
-    decrypted += decipher.final('utf8');
-    return decrypted;
+function decrypt(encryptedText) {
+  return new Promise((resolve, reject) => {
+    const iv = Buffer.from(encryptedText.slice(0, 32), "hex");
+    const decipher = crypto.createDecipheriv("aes-256-cbc", secret, iv);
+    let decrypted = decipher.update(encryptedText.slice(32), "hex", "utf8");
+    decrypted += decipher.final("utf8");
+    resolve(decrypted);
+  });
 }
 
-app.post('/create-jwe', async (req, res) => {
-    try {
-        const tokenData = JSON.stringify(req.body);
-        const encryptedToken = encrypt(tokenData, process.env.SECRET);
-
-        const token = jwt.sign({ encryptedToken }, process.env.JWT_SECRET);
-        res.status(200).json({ token });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+app.post("/create-jwe", async (req, res) => {
+  try {
+    const tokenData = JSON.stringify(req.body);
+    const encryptedToken = await encrypt(tokenData, process.env.SECRET);
+    const token = toJwt(encryptedToken);
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
-app.post('/verify-jwe', async (req, res) => {
-    try {
-        const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-
-        const encryptedToken = decodedToken.encryptedToken;
-        const decryptedToken = decrypt(encryptedToken, process.env.SECRET);
-
-        const decoded = JSON.parse(decryptedToken);
-        res.status(200).json(decoded);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+app.post("/verify-jwe", async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const encryptedToken = fromJwt(token);
+    const decryptedToken = await decrypt(encryptedToken, process.env.SECRET);
+    const decoded = JSON.parse(decryptedToken);
+    res.status(200).json(decoded);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`listening on http://localhost:${port}`);
+  console.log(`listening on http://localhost:${port}`);
 });
