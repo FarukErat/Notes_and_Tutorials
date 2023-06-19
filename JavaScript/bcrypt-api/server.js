3eaf2299dc21679c14d0dbd73f2725c2c8000ca6
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2023-06-19 03:31:42 +0300

    add swagger ui for endpoints

--- JavaScript/bcrypt-api/server.js
+++ JavaScript/bcrypt-api/server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const ip = require("ip");
+const cors = require("cors");
+const YAML = require("yamljs");
+const path = require("path");
+const swaggerUi = require("swagger-ui-express");
 
 const app = express();
 const port = 3013;
 const workFactor = 12;
+const swaggerDocument = YAML.load(path.join(__dirname, "swagger.yaml"));
 
 app.use(express.json());
+app.use(cors());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.post("/hash", async (req, res) => {
   const { password } = req.body;
@@ -24,4 +31,3 @@ app.post("/validate", async (req, res) => {
 app.listen(port, () => {
   console.log(`app listening at http://${ip.address()}:${port}`);
 });
-
