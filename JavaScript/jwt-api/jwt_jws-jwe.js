b287cf1b34a51efec61453e919a60e78446a8043
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2023-06-25 05:24:14 +0300

    Update jwt_jws-jwe.js

--- JavaScript/jwt-api/jwt_jws-jwe.js
+++ JavaScript/jwt-api/jwt_jws-jwe.js
@@ -15,7 +15,7 @@ const fromJwt = (payload) => jwt.verify(payload, jwsSecret);
 app.post("/create-jws", async (req, res) => {
   try {
     const token = toJwt(req.body);
-    res.status(200).json({ token: token });
+    res.status(200).json({ token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
