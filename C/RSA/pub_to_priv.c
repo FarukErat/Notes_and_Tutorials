1321ec659b5dde79c685d164204e576f28e3a250
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2024-05-22 21:06:18 +0300

    Update pub_to_priv.c

--- C/RSA/pub_to_priv.c
+++ C/RSA/pub_to_priv.c
@@ -8,7 +8,7 @@ int main() {
     uint64_t exponent1 = 6427, product = 4168061; // obtain these values from the public key
     uint64_t prime1, prime2; // Prime factors of product (prime1 and prime2 are typically not known in practice)
 
-    prime1 = smallest_prime_factor(product);
+    prime1 = find_smallest_prime_factor(product);
     prime2 = product / prime1;
 
     // Calculate Euler's totient function φ(product)
