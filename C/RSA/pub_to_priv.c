5249227969ad7ceec453461ae706cb95ecf47181
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2024-05-22 22:05:18 +0300

    Update pub_to_priv.c

--- C/RSA/pub_to_priv.c
+++ C/RSA/pub_to_priv.c
@@ -3,22 +3,18 @@
 
 #include "pub_to_priv.h"
 
-int main() {
-    uint64_t exponent1 = 12375, product = 1040839753; // obtain these values from the public key
-    uint64_t prime1, prime2; // Prime factors of product (prime1 and prime2 are typically not known in practice)
+uint64_t main(int argc, char* argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <exponent1> <product>\n", argv[0]);
+        return 0;
+    }
 
-    prime1 = smallest_prime_factor_in_range(product, 2, product);
-    prime2 = product / prime1;
-    printf("Prime 1: %llu\n", prime1);
-    printf("Prime 2: %llu\n", prime2);
+    uint64_t exponent1 = strtoull(argv[1], NULL, 10);
+    uint64_t product = strtoull(argv[2], NULL, 10);
 
-    // Calculate Euler's totient function φ(product)
-    uint64_t phi_n = (prime1 - 1) * (prime2 - 1);
-
-    // Calculate the private key 'exponent2' using the modular inverse of 'exponent1' modulo 'φ(product)'
-    uint64_t exponent2 = mod_inverse(exponent1, phi_n);
+    uint64_t exponent2 = exponent_of_missing_key(exponent1, product);
 
     printf("Private Key (exponent2, product): (%llu, %llu)\n", exponent2, product);
 
-    return 0;
+    return exponent2;
 }
