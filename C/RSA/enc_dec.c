7311de929550847c9c25e26e02fc1ddd39bdda0f
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2024-06-08 13:39:48 +0300

    Update enc_dec.c

--- C/RSA/enc_dec.c
+++ C/RSA/enc_dec.c
@@ -1,3 +1,18 @@
+/*
+Sample Output:
+
+Generated RSA Key Pair:
+Public Key (exponent1, product): (28921, 1071640039)
+Private Key (exponent2, product): (445943401, 1071640039)
+
+Original Message: 987654321
+Encrypted Message (Ciphertext): 86537746
+Decrypted Message: 987654321
+Encrypted Message (Ciphertext): 284691966
+Decrypted Message: 987654321
+*/
+
+
 #include <stdio.h> // For printf
 
 #include "enc_dec.h"
@@ -20,13 +35,24 @@ int main() {
     // Encrypt the message using public key (exponent1, product)
     uint64_t ciphertext = rsa_encrypt(message, exponent1, product);
     if (ciphertext == -1) {
-        printf("Error: Message is too large to be encrypted with the given public key\n");
+        printf("Error: Message is too large to be encrypted with the given key\n");
         return 1;
     }
     printf("Encrypted Message (Ciphertext): %llu\n", ciphertext);
 
     // Decrypt the ciphertext using private key (exponent2, product)
     uint64_t decrypted_message = rsa_decrypt(ciphertext, exponent2, product);
+    printf("Decrypted Message: %llu\n", decrypted_message);
+
+    // Encrypt the message using private key (exponent2, product)
+    ciphertext = rsa_encrypt(message, exponent2, product);    if (ciphertext == -1) {
+        printf("Error: Message is too large to be encrypted with the given key\n");
+        return 1;
+    }
+    printf("Encrypted Message (Ciphertext): %llu\n", ciphertext);
+
+    // Decrypt the ciphertext using public key (exponent1, product)
+    decrypted_message = rsa_decrypt(ciphertext, exponent1, product);
     printf("Decrypted Message: %llu\n", decrypted_message);
 
     return 0;
