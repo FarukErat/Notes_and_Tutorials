9bfb7a17f08f781067b8be6fe0b8b5af868a7846
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2024-09-14 02:57:18 +0300

    Update Program.cs

--- C#/RSA/Program.cs
+++ C#/RSA/Program.cs
@@ -1,23 +1,159 @@
 using System.Numerics;
-using Services;
 
-BigInteger message = 123456789;
-Console.WriteLine($"Message: {message}\n");
+namespace Services;
 
-(BigInteger product, BigInteger exponent1, BigInteger exponent2) = Factorization.GenerateKeys();
+// 9_223_372_036_854_775_783 is the largest prime number that fits in a long (9223372036854775783)
+// to test the performance of the IsPrime method
 
-Console.WriteLine($"Product: {product}");
-Console.WriteLine($"Exponent1: {exponent1}");
-Console.WriteLine($"Exponent2: {exponent2}\n");
+public static class Factorization
+{
+    public static bool IsPrime(BigInteger number)
+    {
+        if (number < 2)
+        {
+            return false;
+        }
 
-BigInteger encrypted = Factorization.Encrypt(message, product, exponent1);
-BigInteger decrypted = Factorization.Encrypt(encrypted, product, exponent2);
+        if (number == 2 || number == 3)
+        {
+            return true;
+        }
 
-Console.WriteLine($"Encrypted with exponent1: {encrypted}");
-Console.WriteLine($"Decrypted with exponent2: {decrypted}\n");
+        if (number % 2 == 0 || number % 3 == 0)
+        {
+            return false;
+        }
 
-encrypted = Factorization.Encrypt(message, product, exponent2);
-decrypted = Factorization.Encrypt(encrypted, product, exponent1);
+        long sqrtN = (long)Math.Sqrt((double)number);
+        object lockObject = new();
+        bool result = true;
+        long count = sqrtN / 6 + 1; // +1 due to integer truncation
 
-Console.WriteLine($"Encrypted with exponent2: {encrypted}");
-Console.WriteLine($"Decrypted with exponent1: {decrypted}\n");
+        Parallel.For(
+            fromInclusive: 1, // 1 since the first candidate is 5
+            toExclusive: count + 1, // +1 due to exclusive upper bound
+            parallelOptions: new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount },
+            body: (i, state) =>
+            {
+                BigInteger k1 = 6 * i - 1;
+                BigInteger k2 = 6 * i + 1;
+
+                if (result
+                    && (number % k1 == 0 || number % k2 == 0)
+                    && number != k1
+                    && number != k2)
+                {
+                    lock (lockObject)
+                    {
+                        result = false;
+                        state.Stop();
+                    }
+                }
+            }
+        );
+
+        return result;
+    }
+
+    static BigInteger GetRandomPrime()
+    {
+        Random random = new();
+        BigInteger number = random.Next(1, int.MaxValue);
+
+        while (!IsPrime(number))
+        {
+            number = random.Next(1, int.MaxValue);
+        }
+
+        return number;
+    }
+
+    static BigInteger GCD(BigInteger a, BigInteger b)
+    {
+        while (b != 0)
+        {
+            BigInteger temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+
+    static BigInteger ModularExponentiation(BigInteger number, BigInteger exponent, BigInteger modulus)
+    {
+        BigInteger result = 1;
+
+        while (exponent > 0)
+        {
+            if (exponent % 2 == 1)
+            {
+                result = (result * number) % modulus;
+            }
+
+            number = (number * number) % modulus;
+            exponent /= 2;
+        }
+
+        return result;
+    }
+
+    static BigInteger ModularInverse(BigInteger a, BigInteger m)
+    {
+        BigInteger m0 = m;
+        BigInteger y = 0, x = 1;
+
+        if (m == 1)
+            return 0;
+
+        while (a > 1)
+        {
+            // q is quotient
+            BigInteger q = a / m;
+            BigInteger t = m;
+
+            // m is remainder now, process same as Euclid's algorithm
+            m = a % m;
+            a = t;
+            t = y;
+
+            // Update x and y
+            y = x - q * y;
+            x = t;
+        }
+
+        // Make x positive
+        if (x < 0)
+            x += m0;
+
+        return x;
+    }
+
+    public static (BigInteger product, BigInteger exponent1, BigInteger exponent2) GenerateKeys(){
+        BigInteger prime1 = GetRandomPrime();
+        BigInteger prime2 = GetRandomPrime();
+        BigInteger product = prime1 * prime2;
+        BigInteger phi = (prime1 - 1) * (prime2 - 1);
+
+        // exponent1 must be coprime with phi
+        BigInteger exponent1 = GetRandomPrime();
+        while (GCD(exponent1, phi) != 1)
+        {
+            exponent1 = GetRandomPrime();
+        }
+
+        BigInteger exponent2 = ModularInverse(exponent1, phi);
+
+        return (product, exponent1, exponent2);
+    }
+
+    public static BigInteger Encrypt(BigInteger message, BigInteger product, BigInteger exponent)
+    {
+        return ModularExponentiation(message, exponent, product);
+    }
+
+    public static BigInteger Decrypt(BigInteger encryptedMessage, BigInteger product, BigInteger exponent)
+    {
+        return ModularExponentiation(encryptedMessage, exponent, product);
+    }
+}
